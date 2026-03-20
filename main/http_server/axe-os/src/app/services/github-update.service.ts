e63bda1dff30289a5038fea1aa9bce1f0677a880
Z3r0XG <18423772+Z3r0XG@users.noreply.github.com>
2026-03-20 19:14:40 -0400

    branding: point update service at heliospool/HexOS releases

--- main/http_server/axe-os/src/app/services/github-update.service.ts
+++ main/http_server/axe-os/src/app/services/github-update.service.ts
@@ -23,7 +23,7 @@ export class GithubUpdateService {
 
   public getReleases(): Observable<GithubRelease[]> {
     return this.httpClient.get<GithubRelease[]>(
-      'https://api.github.com/repos/Z3r0XG/GekkoAxeOS/releases'
+      'https://api.github.com/repos/heliospool/HexOS/releases'
     ).pipe(
       map((releases: GithubRelease[]) => releases.filter((release: GithubRelease) => !release.prerelease))
     );
