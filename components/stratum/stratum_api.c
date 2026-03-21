aed21bf80bc980efd11562f04970116ab291409e
Z3r0XG <18423772+Z3r0XG@users.noreply.github.com>
2026-03-20 20:49:52 -0400

    stratum: restore bitaxe/{asic}/{version} user agent format

--- components/stratum/stratum_api.c
+++ components/stratum/stratum_api.c
@@ -444,7 +444,7 @@ int STRATUM_V1_subscribe(esp_transport_handle_t transport, int send_uid, const c
     const esp_app_desc_t *app_desc = esp_app_get_description();
     const char *version = app_desc->version;	
     snprintf(subscribe_msg, sizeof(subscribe_msg),
-        "{\"id\":%d,\"method\":\"mining.subscribe\",\"params\":[\"%s/%s\"]}\n",
+        "{\"id\":%d,\"method\":\"mining.subscribe\",\"params\":[\"bitaxe/%s/%s\"]}\n",
         send_uid, model, version);
     debug_stratum_tx(subscribe_msg);
 
